46a05af0d41eea8c3c7ee309b7aee8b5d6a6f6a5
Seungil Kim <ksi990302@gmail.com>
2023-10-02 19:45:36 +0900

    전체 디렉토리에 대해 병렬로 실행하도록 변경

--- converter.py
+++ converter.py
@@ -1,40 +1,53 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 from PIL import Image
 from reportlab.pdfgen import canvas
 
-# 현재 디렉토리에 존재하는 PNG 파일을 모두 찾습니다.
-current_directory = os.getcwd()
-png_files = [f for f in os.listdir(current_directory) if f.endswith('.png')]
 
-# 이름에 포함된 숫자 순서로 정렬합니다. 1.png, 2.png, 3.png, ...10.png, 11.png, ...
-png_files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+def create_pdf_from_pngs(directory):
+    output_pdf_file = os.path.join(
+        directory, f"{directory.split(os.path.sep)[-1]}.pdf")
+    pdf_filename = os.path.basename(output_pdf_file)
 
-if not png_files:
-    print("현재 디렉토리에 PNG 파일이 없습니다.")
-    exit()
+    if os.path.exists(output_pdf_file):
+        print(f"{pdf_filename} 파일이 이미 존재합니다.")
+        return
 
-# PNG 파일 목록을 출력합니다.
-for png_file in png_files:
-    print(png_file)
+    png_files = [f for f in os.listdir(directory) if f.endswith('.png')]
+    png_files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
 
-# PDF 파일을 생성합니다. 파일 이름은 현재 디렉토리명으로 합니다.
-output_pdf_file = f"{current_directory.split(os.path.sep)[-1]}.pdf"
-c = canvas.Canvas(output_pdf_file)
+    if not png_files:
+        directory_name = os.path.basename(directory)
+        print(f"{directory_name} 디렉토리에 PNG 파일이 존재하지 않습니다.")
+        return
 
-for png_file in png_files:
-    # PNG 파일을 엽니다.
-    img = Image.open(png_file)
-    img_width, img_height = img.size
+    c = canvas.Canvas(output_pdf_file)
 
-    # PDF 페이지를 PNG 이미지 크기에 맞게 설정합니다.
-    c.setPageSize((img_width, img_height))
-    # PNG 이미지를 PDF에 추가합니다.
-    c.drawInlineImage(png_file, 0, 0, width=img_width, height=img_height)
-    # 다음 페이지를 추가합니다.
-    c.showPage()
+    for png_file in png_files:
+        png_path = os.path.join(directory, png_file)
+        img = Image.open(png_path)
+        img_width, img_height = img.size
 
-# PDF 파일을 저장합니다.
-c.save()
+        c.setPageSize((img_width, img_height))
+        c.drawInlineImage(png_path, 0, 0, width=img_width, height=img_height)
+        c.showPage()
 
-print(f"{output_pdf_file} 파일이 생성되었습니다.")
+    c.save()
+    print(f">>{pdf_filename} 파일이 생성되었습니다.<<")
+
+
+def main(directory):
+    with ThreadPoolExecutor() as executor:
+        # 현재 디렉토리의 모든 항목을 가져옵니다.
+        for item in os.listdir(directory):
+            item_full_path = os.path.join(directory, item)
+
+            # 항목이 디렉토리이고 숨김 폴더가 아니라면 처리합니다.
+            if os.path.isdir(item_full_path) and not item.startswith('.'):
+                executor.submit(create_pdf_from_pngs, item_full_path)
+
+
+if __name__ == '__main__':
+    current_directory = os.getcwd()
+    main(current_directory)
